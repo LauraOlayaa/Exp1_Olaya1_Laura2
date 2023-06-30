ac040a7ad213c69ce8073bffa1bf7c8cf5221e05
Laura Olaya <valenthinah2004@gmail.com>
2023-06-30 02:55:21 -0400

    crud y loggin

--- Laura_oolaya/Laura_oolaya/urls.py
+++ Laura_oolaya/Laura_oolaya/urls.py
@@ -1,9 +1,1 @@
 
-from django.contrib import admin
-from django.urls import path
-from Laura_oolaya.vista import index
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', index)
-]
